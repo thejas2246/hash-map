f40f288d21746bd6666842f960f78084b8a86a0e
thejas <thejas2246@gmail.com>
2025-08-04 21:06:24 +0530

    method to hash key

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-class HashMap{
-    constructor(){
-        this.loadFactor;
-        this.capacity;
+class HashMap {
+  constructor() {
+    this.loadFactor;
+    this.capacity;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i))%16;
     }
+
+    return hashCode;
+  }
+
 }
+
+
+let test = new HashMap();
+
+console.log(test.hash("Rama"));
+console.log(test.hash("sita"));
+console.log(test.hash("jina"));
+console.log(test.hash("jinu"));
+console.log(test.hash("Sita"));
+
+
